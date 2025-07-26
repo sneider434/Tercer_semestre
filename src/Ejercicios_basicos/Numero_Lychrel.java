0c2d8064a8799a1e8e7205ef8a9006731cab190a
sneider434 <darwinlindarte072@gmail.com>
2025-07-26 18:56:17 -0500

    se añade ejercio sobre los numero de lychrel

---
+++ src/Ejercicios_basicos/Numero_Lychrel.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Numero_Lychrel  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("  Ingrese un numero ");
+        int numero_ingresado = scanner.nextInt();
+        int numero_inverso =0;
+        int numero_sumador =numero_ingresado;
+        while(numero_sumador != numero_inverso){
+
+            int numero_guardar = numero_sumador;
+            numero_inverso= 0;
+            while(numero_sumador>0){
+                int digito  = numero_sumador % 10;
+                numero_inverso = (numero_inverso * 10 ) + digito;
+                numero_sumador = numero_sumador / 10 ; 
+            }
+            numero_sumador= numero_guardar;
+            System.out.println(numero_guardar);
+            if(numero_sumador==numero_inverso){
+                System.out.println("---------------------------------------");
+                System.out.println(" no es un numero de Lychrel");
+                System.out.println("---------------------------------------");
+                break;
+            }else{
+                 numero_sumador+=numero_inverso;
+            }
+            
+           
+            
+        }
+       
+
+        scanner.close();
+    }
+}
--- src/Ejercicios_basicos/Numero_perfecto.java
+++ src/Ejercicios_basicos/Numero_perfecto.java
@@ -5,16 +5,16 @@ public class Numero_perfecto {
         Scanner scanner = new Scanner(System.in);
         System.out.println(" ingrese un numero entero positivo");
         int numero = scanner.nextInt();
-        int parte_entera = (int)Math.sqrt(numero);
-        int[][] multiplos = new int[parte_entera][2];
+        int parte_entera = (int)Math.sqrt(numero);// esta variable es para saber la cantidad de posibles multiplos del numero x
+        int[][] multiplos = new int[parte_entera][2]; // arreglo para almacenar los numeros que multiplicados son el numero x
         int i = 1;
         int x = 1;
-        int posicion = 0 ;
+        int posicion = 0 ; // variable bandera para almecenar el arreglo 
         // este bucle encuentra los numeros que multipplicados den el numero ingresado 
         while (i <=  numero) {
             x=1;
             while (x<=numero) {
-                if(i*x == numero && i<=x){
+                if(i*x == numero && i<=x){ // aqui se veifica si el numero es igual al numero x y verifica que no se repitan numeros
                     multiplos[posicion][0]= i;
                     multiplos[posicion][1] = x; 
                     posicion = posicion + 1;
@@ -24,14 +24,14 @@ public class Numero_perfecto {
             i=i+1;
         }
     
-        int verificador_numero_perfecto = 0 ;
+        int verificador_numero_perfecto = 0 ; // variable de suma para saber si es numero perfecto
 
         for(int k =0;k<parte_entera;k++){
             for( int j = 0; j < 2;j++){
-                if( multiplos[k][j]== numero){
+                if( multiplos[k][j]== numero){ // este if elimina de los numeros eliminados es el numero x ya que este no es valido
                     multiplos[k][j]=0;
                 }
-                verificador_numero_perfecto += multiplos[k][j];
+                verificador_numero_perfecto += multiplos[k][j];// sumador de los numeros almacenados
             }
         }
   
